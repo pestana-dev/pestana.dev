f266babb058f75666e8920fbbb43516f5a14f144
Gabriela <gabrielasap19@gmail.com>
2025-08-30 19:22:02 -0300

    chore: change `GihubRepos.tsx` file name

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,7 +6,7 @@ import Projects from "@/components/Projects";
 import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
-import GitHubRepos from "@/components/GithupRepos";
+import GitHubRepos from "@/components/GithubRepos";
 
 const Index = () => {
   return (
